0759e941d9dec077925182743f96cb7744f36502
Borislav Mihaylov <borvalorant@gmail.com>
2024-12-09 20:08:43 +0200

    guard

--- gamings/src/app/guards/auth.guard.ts
+++ gamings/src/app/guards/auth.guard.ts
@@ -1,31 +1,37 @@
 import { inject } from '@angular/core';
 import {
-  CanActivateFn,
   ActivatedRouteSnapshot,
-  RouterStateSnapshot,
+  CanActivateFn,
   Router,
+  RouterStateSnapshot,
   UrlTree,
 } from '@angular/router';
-import { UserService } from '../user.service';
-import { map, take } from 'rxjs';
-
+import { UserService } from '../user/user.service';
+import { map, take, catchError, of } from 'rxjs';
+import { User } from '../models/user';
 
 export const authGuard: CanActivateFn = (
   route: ActivatedRouteSnapshot,
   state: RouterStateSnapshot
 ) => {
-  const userService = inject(UserService); // Use dependency injection
-  const router = inject(Router); // Use dependency injection
+  const userService = inject(UserService);
+  const router = inject(Router);
 
-  return userService.isLoggedIn$.pipe(
+  return userService.getCurrentUser().pipe(
     take(1),
-    map((isLoggedIn) => {
-      if (isLoggedIn) {
+    map((user) => {
+      if (user) {
         return true;
       } else {
         router.navigate(['/login'], { replaceUrl: true });
         return false;
       }
+    }),
+    catchError((error) => {
+      console.error('Error in AuthGuard:', error);
+      // Handle the error appropriately (e.g., redirect to an error page)
+      router.navigate(['/error']); //Example
+      return of(false);
     })
   );
 };
