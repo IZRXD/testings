31555dfee12a17502a68eaae58d458a3193641cd
Borislav Mihaylov <borvalorant@gmail.com>
2024-12-11 17:16:18 +0200

    fixes

--- gamings/src/app/game-details/game-details.component.ts
+++ gamings/src/app/game-details/game-details.component.ts
@@ -1,5 +1,5 @@
 // game-details.component.ts
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { Game } from '../models/game';
 import { ApiService } from '../api.service';
@@ -18,44 +18,38 @@ import { Location } from '@angular/common';
 export class GameDetailsComponent implements OnInit {
   game: Game | null = null;
   isLoading: boolean = true;
-  isInit: boolean = false; // Add this flag
 
   constructor(
     private route: ActivatedRoute,
     private apiService: ApiService,
-    public gameService: GameService,
+    @Inject(GameService) public gameService: GameService,
 
     private location: Location
   ) {}
 
   ngOnInit(): void {
-    if (!this.isInit) {
-      // Check if component is initialized
-      this.isInit = true; // Set flag to true
-      const id = this.route.snapshot.params['gameId'];
-      this.apiService.getSingleGame(id).subscribe({
-        next: (game) => {
-          this.gameService.setGame(game);
-          this.game = game;
-          this.isLoading = false;
-        },
-        error: (error) => {
-          this.isLoading = false;
-          console.error('Error fetching game:', error);
-        },
-      });
-    }
+    const id = this.route.snapshot.params['gameId'];
+    this.apiService.getSingleGame(id).subscribe({
+      next: (game) => {
+        this.gameService.setGame(game);
+        this.game = game;
+        this.isLoading = false;
+      },
+      error: (error) => {
+        this.isLoading = false;
+        console.error('Error fetching game:', error);
+      },
+    });
   }
 
   deleteGame() {
     console.log(this.game);
-    
-    
+
     if (this.game && this.game._id) {
       this.apiService.deleteGame(this.game._id).subscribe({
         next: () => {
-          console.log('Game deleted successfully.',this.game);
-          
+          console.log('Game deleted successfully.', this.game);
+
           this.location.back(); // Navigate back to catalog
         },
         error: (error) => {
